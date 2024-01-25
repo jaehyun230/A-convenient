c9d6d25349836ac6a7655660df58177b1c88e56f
jaehyun230 <wogus230vvv@gmail.com>
2024-01-25 17:47:29 +0900

    [feat] Cookie Path 설정 부분 추가

--- src/main/java/myProject/service/cookie/util/CookieUtill.java
+++ src/main/java/myProject/service/cookie/util/CookieUtill.java
@@ -3,6 +3,7 @@ package myProject.service.cookie.util;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Cookie;
+import myProject.service.cookie.dto.QueryDTO;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -25,7 +26,10 @@ public class CookieUtill {
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
+                System.out.println("cookie name = " + cookie.getName());
                 if (cookieName.equals(cookie.getName())) {
+
+                    System.out.println("cookie find = " + cookie);
                     return cookie.getValue();
                 }
             }
@@ -38,12 +42,19 @@ public class CookieUtill {
      * 새로운 UUID 값을 가진 'SuperOfficeCookie' 이름의 쿠키를 생성하고,
      * 정해진 만료 시간(ExpiredTimeSeconds)으로 설정한 후 HTTP 응답에 추가합니다.
      */
-    public String createCookie(HttpServletResponse response) {
+    public String createCookie(QueryDTO queryDTO, HttpServletResponse response) {
+
         String uuid_cookie = UUID.randomUUID().toString();
-        Cookie cookie = new Cookie("SuperOfficeCookie", uuid_cookie);
-        cookie.setMaxAge(ExpiredTimeSeconds);
-        response.addCookie(cookie);
-        return cookie.getValue();
+
+        // 쿠키 문자열을 직접 조합합니다. 여기에 Path 설정을 포함시킵니다.
+        String cookieString = String.format("A-Convenient-image=%s; Max-Age=%d; Path=/; HttpOnly; Secure; SameSite=None",
+                uuid_cookie, CookieUtill.ExpiredTimeSeconds);
+
+        // 'Set-Cookie' 헤더에 쿠키 문자열을 추가합니다.
+        response.setHeader("Set-Cookie", cookieString);
+
+        System.out.println("create Cookie end and Cookie String = " + cookieString);
+        return uuid_cookie;
     }
 
 }
